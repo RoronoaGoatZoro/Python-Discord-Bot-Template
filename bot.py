7c807b84464c1feb7bb21dc0d633caf32234e690
RoronoaGoatZoro <125669012+RoronoaGoatZoro@users.noreply.github.com>
2023-02-17 14:15:34 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,14 +27,8 @@ else:
     with open(f"{os.path.realpath(os.path.dirname(__file__))}/config.json") as file:
         config = json.load(file)
 
-"""	
-Setup bot intents (events restrictions)
-For more information about intents, please go to the following websites:
-https://discordpy.readthedocs.io/en/latest/intents.html
-https://discordpy.readthedocs.io/en/latest/intents.html#privileged-intents
 
 
-Default Intents:
 intents.bans = True
 intents.dm_messages = True
 intents.dm_reactions = True
@@ -54,20 +48,14 @@ intents.typing = True
 intents.voice_states = True
 intents.webhooks = True
 
-Privileged Intents (Needs to be enabled on developer portal of Discord), please use them only if you need them:
 intents.members = True
 intents.message_content = True
 intents.presences = True
-"""
+
 
 intents = discord.Intents.default()
 
-"""
-Uncomment this if you want to use prefix (normal) commands.
-It is recommended to use slash commands and therefore not use prefix commands.
 
-If you want to use prefix commands, make sure to also enable the intent below in the Discord developer portal.
-"""
 # intents.message_content = True
 
 bot = Bot(command_prefix=commands.when_mentioned_or(
